ecb82e50e77641e8fa5b76576c59b1f7073e7409
anton <antonlaukemper@yahoo.de>
2018-02-14 19:39:29 +0100

    Add evaluate method and make it more modular

--- anomaly_detection_service.py
+++ anomaly_detection_service.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
 import pickle
+
 import numpy as np
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 rf_model = pickle.load(open('random_forest_18.pkl', 'rb'))
@@ -10,11 +10,18 @@ rf_model = pickle.load(open('random_forest_18.pkl', 'rb'))
 # handpicked:
 thresholds = {'Influenza': 853, 'Windpocken': 178, 'Norovirus-Gastroenteritis': 1800}
 
-@app.route('/predict', methods=['POST'])
-def predict():
+def predict(data_json):
+    """
+    given the current distribution of symptoms, this method returns the estimated distribution of diseases
+    """
     if request.method == 'POST':
-        data_json = request.json
-        data = np.array(list(data_json['symptoms'].values())).reshape(1, -1) 
+        
+        key_list = list(data_json['symptoms'].keys())
+        key_list.sort()
+        data = np.zeros([len(key_list)])
+        for i, key in enumerate(key_list):
+            data[i] = data_json['symptoms'][key]
+        data = data.reshape(1, -1)
         print("This is the data put into the model: "+str(data)+ " "+str(type(data))+ " "+str(data.shape))
         prediction = rf_model.predict(data)
         prediction_dict = {
@@ -23,11 +30,39 @@ def predict():
             'Norovirus-Gastroenteritis': prediction[0][2]
         }
 
-        print("hallo")
+    return prediction_dict
 
-    return jsonify(prediction_dict)
+def threshold_report(data):
+    """
+    given the current distribution of diseases, this method returns a json with a truth value for every given disease. True means that the number of cases
+    is over a specific value that we determined as report-worthy
+    """
+    if request.method == 'POST':
+        report = {}
+        
+        for key in data.keys():
+            if data[key]>thresholds[key]:
+                report[key] = True
+            else:
+                report[key] = False
+    return report
 
-# @app.route('/evaluate', methods=['POST'])
-# def predict():
-#     if request.method == 'POST':
-#         data_json = request.json
+
+
+@app.route('/predict', methods=['POST'])
+def give_prediction():
+    data_json = request.json
+    return jsonify(predict(data_json))
+
+
+
+
+@app.route('/evaluate', methods=['POST'])
+def evaluate():
+    data_json = request.json
+    data = predict(data_json)
+    return jsonify(threshold_report(data))
+
+
+
+
